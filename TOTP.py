5e09d7b34943e4ef0925d99f2110c31b77f83ab9
Anthony Lee <leeswlwpl@gmail.com>
2021-05-28 14:48:23 +0800

    Update TOTP.py

--- TOTP.py
+++ TOTP.py
@@ -1,8 +1,18 @@
 # The algorithm of TOTP is similar to HOTP
-# The difference is replacing the following codes to change the counter calculation:
 
+
+# The difference is replacing the following codes to change the counter calculation (in both clientside and serverside):
 interval = 30
 time_elapse = time.time() 
-interval_count = int(time_elapse // interval)
-counter = bytes(interval_count)
+counter = int(time_elapse // interval)
 
+
+# And change the valid window (in serverside):
+counter -= 5
+for i in range(0, 11):
+    OTP_pw = OTP(KEY, counter, 6)
+    if inputCode == OTP_pw:
+        print("Succuess authentication!")
+        break
+        
+        
